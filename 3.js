002680a2e6b41dd8dbd6934e0bbcf3711e7bd91a
lLeahDizon <516330797@qq.com>
2021-09-24 17:20:14 +0800

    feature: stream 对象的原型、事件

--- 3.js
+++ 3.js
@@ -1,8 +1,15 @@
 const http = require('http')
 const fs = require('fs')
 const server = http.createServer()
-server.on('request',(request,response)=>{
+server.on('request', (request, response) => {
   const stream = fs.createReadStream('./big_file.txt')
+  stream.on('data', (chunk) => {
+    console.log('读取了一次数据')
+    console.log(chunk.toString())
+  })
+  stream.on('end', () => {
+    console.log('全部读完了')
+  })
   stream.pipe(response)
 })
 
