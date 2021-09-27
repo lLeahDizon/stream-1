8c731f6a0613383fab8b8399d1a0aef162cb417f
lLeahDizon <516330797@qq.com>
2021-09-27 19:17:17 +0800

    feature: 调用 api

--- 3.js
+++ 3.js
@@ -3,14 +3,11 @@ const fs = require('fs')
 const server = http.createServer()
 server.on('request', (request, response) => {
   const stream = fs.createReadStream('./big_file.txt')
-  stream.on('data', (chunk) => {
-    console.log('读取了一次数据')
-    console.log(chunk.toString())
-  })
-  stream.on('end', () => {
-    console.log('全部读完了')
-  })
   stream.pipe(response)
+  stream.pause()
+  setTimeout(() => {
+    stream.resume()
+  }, 3000)
 })
 
 server.listen(8888)
